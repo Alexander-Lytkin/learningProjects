82d6caf4a2fe35d371cd4c51fdd913d69397643a
Alexander Lytkin <a.lytkin@omprussia.ru>
2020-08-19 09:47:00 +0300

    Task11

--- tasks/task_11/program.py
+++ tasks/task_11/program.py
@@ -4,5 +4,12 @@
 """
 
 
-def func(l):
-    pass
+def func(le):
+    dict_le = {}
+    for value in le:
+        if not dict_le.get(value):
+            dict_le[value] = 1
+        else:
+            dict_le[value] += 1
+    values = dict_le.items()
+    return [key for key, value in values if value == 1]
