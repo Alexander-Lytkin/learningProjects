4ddaea861715cbf5fd5b9e5db52013b4b5268eaf
Alexander Lytkin <a.lytkin@omprussia.ru>
2020-08-19 09:46:36 +0300

    Task10

--- tasks/task_10/program.py
+++ tasks/task_10/program.py
@@ -5,4 +5,17 @@
 
 
 def func(s):
-    pass
+    i = 0
+    list_s = []
+    while i < len(s):
+        if s[i].isdigit():
+            temp_str = ""
+            while i < len(s):
+                temp_str += s[i]
+                i += 1
+                if not s[i].isdigit():
+                    list_s.append(int(temp_str))
+                    break
+        else:
+            i += 1
+    return list_s
