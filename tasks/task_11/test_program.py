6e1fb51d1e628cb5c16f062a5e2ab18232e44835
Alexander Lytkin <a.lytkin@omprussia.ru>
2020-08-19 09:47:00 +0300

    Task11

--- tasks/task_11/test_program.py
+++ tasks/task_11/test_program.py
@@ -3,13 +3,14 @@ from tasks.task_11.program import func
 
 
 @pytest.mark.parametrize(
-    "l,result",
+    "le,result",
     [
         ([], []),
         ([1, 1], []),
+        ([1, 1, 1], []),
         ([1, 2, 1], [2]),
         (["a", 1, 2, "1", "a", "ab", 2], [1, "1", "ab"]),
     ],
 )
-def test_func(l, result):
-    assert func(l) == result
+def test_func(le, result):
+    assert func(le) == result
