e78604c15cc076e815c049842bba47f292090fff
Alexander Lytkin <a.lytkin@omprussia.ru>
2020-09-02 17:51:16 +0300

    TaskAccount2

--- bank/tests/test_account.py
+++ bank/tests/test_account.py
@@ -1,108 +1,78 @@
 import pytest
-from bank.account import Account
 
 
 class TestAccount:
-    def test_newly_opened_account_has_zero_balance(self):
-        account = Account()
-        account.open()
-        assert account.get_balance() == 0
+    def test_open_account(self, opened_account):
+        assert opened_account.is_opened
 
-    def test_open_account(self):
-        account = Account()
-        account.open()
-        assert account.is_opened
+    def test_new_account_has_zero_balance(self, opened_account):
+        assert opened_account.get_balance() == 0
 
-    def test_open_already_opened_account(self):
-        account = Account()
-        account.open()
+    def test_open_already_opened_account(self, opened_account):
         with pytest.raises(ValueError):
-            account.open()
+            opened_account.open()
 
-    def test_close_opened_account(self):
-        account = Account()
-        account.open()
-        account.close()
-        assert not account.is_opened
+    def test_close_opened_account(self, opened_account):
+        opened_account.close()
+        assert not opened_account.is_opened
 
-    def test_close_already_closed_account(self):
-        account = Account()
+    def test_close_already_closed_account(self, new_account):
         with pytest.raises(ValueError):
-            account.close()
+            new_account.close()
 
-    def test_open_closed_account(self):
-        account = Account()
-        account.open()
-        account.deposit(10)
-        account.close()
-        account.open()
-        assert account.is_opened
-        assert account.get_balance() == 10
+    def test_open_closed_account(self, closed_account):
+        closed_account.open()
+        closed_account.deposit(120)
+        assert closed_account.is_opened
+        assert closed_account.get_balance() == 120
 
-    def test_get_balance_if_account_is_closed(self):
-        account = Account()
+    def test_get_balance_if_account_is_closed(self, new_account):
         with pytest.raises(ValueError):
-            account.get_balance()
+            new_account.get_balance()
 
-    def test_get_balance_if_account_is_opened(self):
-        account = Account()
-        account.open()
-        assert account.get_balance() == 0
+    def test_get_balance_if_account_is_opened(self, opened_account):
+        assert opened_account.get_balance() == 0
 
-    def test_get_balance_after_deposit(self):
-        account = Account()
-        account.open()
-        account.deposit(120)
-        assert account.get_balance() == 120
+    def test_get_balance_after_deposit(self, opened_account):
+        opened_account.deposit(20)
+        assert opened_account.get_balance() == 20
 
-    def test_get_balance_after_withdraw(self):
-        account = Account()
-        account.open()
-        account.deposit(150)
-        account.withdraw(120)
-        assert account.get_balance() == 30
+    def test_get_balance_after_withdraw(self, opened_account):
+        opened_account.deposit(120)
+        opened_account.withdraw(50)
+        assert opened_account.get_balance() == 70
 
-    def test_deposit_if_account_is_closed(self):
-        account = Account()
+    def test_deposit_if_account_is_closed(self, new_account):
         with pytest.raises(ValueError):
-            account.deposit(10)
+            new_account.deposit(10)
 
-    def test_deposit_with_negative_amount(self):
-        account = Account()
-        account.open()
+    def test_deposit_with_negative_amount(self, opened_account):
         with pytest.raises(ValueError):
-            account.deposit(-1)
-        assert account.get_balance() == 0
+            opened_account.deposit(-1)
+        assert opened_account.get_balance() == 0
 
-    def test_deposit_for_closed_account(self):
-        account = Account()
+    def test_deposit_for_closed_account(self, new_account):
         with pytest.raises(ValueError):
-            account.deposit(120)
+            new_account.deposit(120)
 
-    def tests_add_deposit_if_account_is_opened(self):
-        account = Account()
-        account.open()
-        account.deposit(120)
-        assert account.get_balance() == 120
+    def tests_add_deposit_if_account_is_opened(self, opened_account):
+        opened_account.deposit(120)
+        assert opened_account.get_balance() == 120
 
-    def test_withdraw_from_opened_account_if_balance_is_below_zero(self):
-        account = Account()
-        account.open()
+    def test_withdraw_from_opened_account_if_balance_is_below_zero(
+        self, opened_account
+    ):
         with pytest.raises(ValueError):
-            account.withdraw(120)
-        assert account.get_balance() == 0
+            opened_account.withdraw(120)
+        assert opened_account.get_balance() == 0
 
-    def test_withdraw_from_opened_account(self):
-        account = Account()
-        account.open()
-        account.deposit(120)
-        account.withdraw(70)
-        assert account.get_balance() == 50
+    def test_withdraw_from_opened_account(self, opened_account):
+        opened_account.deposit(120)
+        opened_account.withdraw(50)
+        assert opened_account.get_balance() == 70
 
-    def test_withdraw_negative_amount(self):
-        account = Account()
-        account.open()
-        account.deposit(10)
+    def test_withdraw_negative_amount(self, opened_account):
+        opened_account.deposit(120)
         with pytest.raises(ValueError):
-            account.withdraw(-1)
-        assert account.get_balance() == 10
+            opened_account.withdraw(-1)
+        assert opened_account.get_balance() == 120
