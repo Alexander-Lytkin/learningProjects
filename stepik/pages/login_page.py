77898029f24f1ed0ca1fd7351d918ff976e05bca
Alexander Lytkin <a.lytkin@omprussia.ru>
2020-10-11 13:55:47 +0300

    lesson_4.3

---
+++ stepik/pages/login_page.py
@@ -0,0 +1,27 @@
+from pages.base_page import BasePage
+from selenium.webdriver.common.by import By
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(By.CSS_SELECTOR, "#login_link"), (
+            "Login link " "is not " "presented "
+        )
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(
+            By.CSS_SELECTOR, "#login_form"
+        ), "Login form is not presented"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(
+            By.CSS_SELECTOR, "#register_form"
+        ), "Register form is not presented"
